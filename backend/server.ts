b5c18cdb85497abd64cae9d51d0838c7d0d06cec
Amelthedevops <amelemal12@gmail.com>
2022-09-13 17:53:15 -0700

    doc(template): add pr and issue template

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.use('/api', require(route/route.ts))
+app.use('/api', require("route/route.ts"))
 app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is running on port${PORT}`)
 })
