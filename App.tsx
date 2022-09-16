093a6079f023f108ff344ac1fd1758e54e60e142
William Brand <wbrand90@gmail.com>
2022-09-16 10:51:46 -0700

    Search layout design complete

--- App.tsx
+++ App.tsx
@@ -3,15 +3,28 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import Search from './screens/search_screen/search';
 
+import { StyleSheet, View } from 'react-native';
+
 export default function App() {
   const [city, setCity] = useState("");
   return (
-    <Provider store={store}>
-      <Search
-        setCity={setCity}
-        city={city}
-      />
-    </Provider>
+
+    <View style={styles.container}>
+      <Provider store={store}>
+        <Search
+          setCity={setCity}
+          city={city}
+        />
+      </Provider>
+    </View>
+
 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+})
--- screens/search_screen/search.tsx
+++ screens/search_screen/search.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FC } from 'react';
-import { Text, View, StyleSheet, TextInput, Image, Button } from 'react-native'
+import { Text, View, StyleSheet, TextInput, Image, Button, Pressable } from 'react-native'
 
 import { Ionicons } from '@expo/vector-icons';
 
@@ -12,19 +12,30 @@ interface Props {
 const Search: FC<Props> = (props) => {
   return (
     <View style={styles.container}>
-      <Ionicons style={styles.locationIcon} name="ios-location-outline" size={24} color="black" />
+      <Pressable style={styles.locationIcon} >
+        <Ionicons name="ios-location-outline" size={34} color="black" />
+      </Pressable>
+
       <Image
         style={styles.logo}
         source={require("../../assets/logo.png")}
       />
       <Text style={styles.logoText}>AirOMeter</Text>
-      <  TextInput
-        placeholder='Enter City Name'
-        //onChangeText={text => setCity(text)}
-        onChangeText={props.setCity}
-        value={props.city}
-      />
-      <Button title='Search' />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Enter City Name'
+          //onChangeText={text => setCity(text)}
+          onChangeText={props.setCity}
+          value={props.city}
+          style={styles.input}
+        />
+        <Pressable style={styles.searchIcon}>
+          <Ionicons name="ios-search" size={24} color="black" />
+        </Pressable>
+
+      </View>
+
+      <Pressable style={styles.button}><Text style={styles.buttonText}>Search</Text></Pressable>
     </View>
   )
 };
@@ -35,18 +46,58 @@ export default Search;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: "100%",
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   locationIcon: {
     alignSelf: 'flex-end',
+    marginRight: 20,
+    marginTop: 20,
   },
   logo: {
-    height: 50,
-    width: 50,
+    height: 150,
+    width: 150,
+    marginTop: 40,
   },
   logoText: {
     fontSize: 30,
-  }
-
+    marginBottom: 50,
+  },
+  input: {
+    height: 40,
+    width: "80%",
+    marginVertical: 20,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    padding: 10,
+  },
+  searchIcon: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    padding: 6,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#19C3FB',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
 })
